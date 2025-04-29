0d045cfb8b271200bc89fd9c8352e96771330648
Rebecca Eifler <eifler@cs.uni-saarland.de>
2025-04-29 16:17:55 +0200

    update body cancel iteration step

--- src/routes/iteration-step.ts
+++ src/routes/iteration-step.ts
@@ -50,6 +50,84 @@ iterationStepRouter.get('/:id', authAny, async (req, res) => {
     }
 });
 
+
+iterationStepRouter.post('/cancel', authAny, async (req, res) => {
+
+    try {
+
+        const id = string().parse(req.body.id);
+        console.log('Cancel: ' + id);
+
+        const step = await IterationStepModel.findById(id);
+
+        if (!step) {
+            res.status(404).send({ message: 'No step found.' });
+            return;
+        }
+
+        if (!step.plan) {
+            res.status(404).send({ message: 'No step found.' });
+            return;
+        }
+
+        const forwardCancelToPlanningService = step.plan.status == PlanRunStatus.RUNNING;
+        console.log("Plan status: " + step.plan.status);
+        
+
+        step.status = StepStatus.UNKNOWN;
+        step.plan.status = PlanRunStatus.CANCELED;
+        await step.save();
+
+        if(forwardCancelToPlanningService){
+            console.log("forward cancel to planning service");
+            let project = await ProjectModel.findById(step.project) as Project;
+            if(!project){
+                project = await DemoModel.findById(step.project) as Project;
+            }
+    
+            if (!project) {
+                console.log('[Cancel Plan Computation] Project does not exist.')
+                step.plan.status = PlanRunStatus.FAILED;
+                await step.save();
+                res.status(200).send({status: false, message:'Project does not exists.'});
+                return;
+            }
+    
+            const services: Service[] = [];
+            for(const serviceId of project.settings.services.services) {
+                const service = await ServiceModel.findById(serviceId);
+                if(service && service.type == ServiceType.PLANNER){
+                    services.push(service);
+                }
+            }
+    
+            if (services.length === 0) {
+                console.log('[Cancel Plan Computation] No selected planner service selected.')
+                step.plan.status = PlanRunStatus.FAILED;
+                await step.save();
+                res.status(200).send({status: false, message: 'No existing planner service selected.'});
+                return;
+            }
+    
+            const success = await callServices(services, JSON.stringify({id}), '/cancel');
+            if(!success){
+                step.plan.status = PlanRunStatus.FAILED;
+                await step.save();
+                console.log('[Cancel Plan Computation] No selected planner service reachable.');
+                res.status(201).send({status: false, message:'No selected planner service reachable.'});
+                return;
+            }
+        }
+
+    
+        res.send(true);
+    } catch (ex) {
+        res.status(500);
+    }
+
+});
+
+
 iterationStepRouter.post('', authAny, async (req: AuthenticatedRequest, res) => {
 
     try {
@@ -91,82 +169,6 @@ iterationStepRouter.post('', authAny, async (req: AuthenticatedRequest, res) => 
     }
     catch (ex) {
         console.log(ex);
-        res.status(500);
-    }
-
-});
-
-
-iterationStepRouter.post('/cancel', authAny, async (req, res) => {
-
-    try {
-
-        const id = string().parse(req.body);
-        console.log('Cancel: ' + id);
-
-        const step = await IterationStepModel.findById(id);
-
-        if (!step) {
-            res.status(404).send({ message: 'No step found.' });
-            return;
-        }
-
-        if (!step.plan) {
-            res.status(404).send({ message: 'No step found.' });
-            return;
-        }
-
-        const forwardCancelToPlanningService = step.plan.status == PlanRunStatus.RUNNING;
-
-        step.status = StepStatus.UNKNOWN;
-        step.plan.status = PlanRunStatus.CANCELED;
-        await step.save();
-
-        if(forwardCancelToPlanningService){
-            let project = await ProjectModel.findById(step.project) as Project;
-            if(!project){
-                project = await DemoModel.findById(step.project) as Project;
-            }
-    
-            if (!project) {
-                console.log('[Cancel Plan Computation] Project does not exist.')
-                step.plan.status = PlanRunStatus.FAILED;
-                await step.save();
-                res.status(200).send({status: false, message:'Project does not exists.'});
-                return;
-            }
-    
-            const services: Service[] = [];
-            for(const serviceId of project.settings.services.services) {
-                const service = await ServiceModel.findById(serviceId);
-                if(service && service.type == ServiceType.PLANNER){
-                    services.push(service);
-                }
-            }
-    
-            if (services.length === 0) {
-                console.log('[Cancel Plan Computation] No selected planner service selected.')
-                step.plan.status = PlanRunStatus.FAILED;
-                await step.save();
-                res.status(200).send({status: false, message: 'No existing planner service selected.'});
-                return;
-            }
-    
-            const success = await callServices(services, JSON.stringify({id}), '/cancel');
-            if(!success){
-                step.plan.status = PlanRunStatus.FAILED;
-                await step.save();
-                console.log('[Cancel Plan Computation] No selected planner service reachable.');
-                res.status(201).send({status: false, message:'No selected planner service reachable.'});
-                return;
-            }
-        }
-
-    
-        res.send({
-            data: {canceled: true}
-        });
-    } catch (ex) {
         res.status(500);
     }
 
